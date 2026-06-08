af84fde5f3e75c9b3d776e1df564c589834fd2e4
NITIN SARDHANA <nitin.sardhana@gmail.com>
2026-06-08 18:23:35 +0530

    Add security groups and EC2 infrastructure

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,3 +22,25 @@ module "vpc" {
   private_subnet_a_cidr = "10.0.11.0/24"
   private_subnet_b_cidr = "10.0.12.0/24"
 }
+
+module "security_groups" {
+  source = "../../modules/security-groups"
+
+  vpc_id      = module.vpc.vpc_id
+  environment = "dev"
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  environment = "dev"
+
+  ami_id        = "ami-0067526cb10a5b138"
+  instance_type = "t3.micro"
+
+  subnet_id = module.vpc.public_subnet_a_id
+
+  security_group_id = module.security_groups.web_security_group_id
+
+  key_name = "travelgo-dev-key-v2"
+}
